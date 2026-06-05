fc29237ed9d853ce25382a0d89b6ba5c8822b790
Abdul Haq <abdulhaq.swe@gmail.com>
2026-06-05 15:45:31 +0500

    fix: allow CORS for Vercel frontend

--- project3/server.js
+++ project3/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { getDb } = require('./database/db');
 const usersRouter = require('./routes/users');
 
+app.use(cors({
+  origin: ['https://decodelabs-tasks-six.vercel.app', 'http://localhost:3000']
+}));
 app.use(express.json());
 app.use('/users', usersRouter);
 
